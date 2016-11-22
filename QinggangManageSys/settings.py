7ee79a15bd4ceac699fd58b17e15378fcf725173
changxin cheng <changxin cheng>
2016-11-22 16:43:59 +0800

    update .gitignore

--- QinggangManageSys/settings.py
+++ QinggangManageSys/settings.py
@@ -11,8 +11,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 import os
+from . import choose_settings
 
 ROW_NUM=10000
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = choose_settings.DEBUG
+TEMPLATE_DEBUG = DEBUG
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Static files (CSS, JavaScript, Images)
@@ -31,10 +36,19 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'data_import/media')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '@o$#tz@^1q8-(nk*8d(-r-pqdbci@*x2(hemnwnvp1&kw$@ggp'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+TEMPLATE_DIRS = choose_settings.TEMPLATE_DIRS
 
-ALLOWED_HOSTS = []
+# See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = choose_settings.ALLOWED_HOSTS
+
+ADMINS = (
+    # ('Your Name', 'your_email@example.com'),
+    ('maksim', 'ccxysfh1993@gmail.com'),
+)
+
+MANAGERS = ADMINS
+
+DATABASES = choose_settings.DATABASES
 
 
 # Application definition
@@ -83,55 +97,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'QinggangManageSys.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
-DATABASES = {
-    '1default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ENGINE': 'django.db.backends.oracle',
-        'NAME': 'qinggang',
-        'USER': 'qinggang',
-        'PASSWORD': 'qinggang',
-        'HOST': '10.30.0.152',
-        'PORT': '1521',
-    },
-    'qg': {
-        'ENGINE': 'django.db.backends.oracle',
-        'NAME': 'maksim',
-        'USER': 'changxin',
-        'PASSWORD': '123456',
-        'HOST': 'localhost',
-        'PORT': '1521',
-    },
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'qinggang',
-        'USER': 'root',
-        'PASSWORD': '123456',
-        # 'HOST': '10.30.0.152',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    },
-    'mes': {
-        'ENGINE': 'django.db.backends.oracle',
-        'NAME': 'mesdb2',#sid:mesdb2;service:mesdb
-        'USER': 'BD_query',
-        'PASSWORD': 'BD_query',
-        'HOST': '10.30.0.17',
-        'PORT': '1521',
-    },
-    'l2': {
-        'ENGINE': 'django.db.backends.oracle',
-        'NAME': 'qgil2db',
-        'USER': 'BD_query',
-        'PASSWORD': 'BD_query',
-        'HOST': '10.20.0.22',
-        'PORT': '1521',
-    }
-}
-
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
 
@@ -151,34 +116,44 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/1.9/topics/i18n/
+# Local time zone for this installation. Choices can be found here:
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# although not all choices may be available on all operating systems.
+# In a Windows environment this must be set to your system time zone.
+TIME_ZONE = 'Asia/Shanghai'
 
+# Language code for this installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+SITE_ID = 1
 
+# If you set this to False, Django will make some optimizations so as not
+# to load the internationalization machinery.
 USE_I18N = True
 
+# If you set this to False, Django will not format dates, numbers and
+# calendars according to the current locale.
 USE_L10N = True
 
+# If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/var/www/example.com/media/"
+MEDIA_ROOT = choose_settings.MEDIA_ROOT
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://example.com/media/", "http://media.example.com/"
+MEDIA_URL = choose_settings.MEDIA_URL
 
-STATIC_URL = '/static/'
-
-
-'''
-自己添加
-'''
-#@login_required,redirect
-LOGIN_URL = '/login'
-
-ADMINS = (
-    ('Maksim Cheng', 'ccxysfh1993@gmail.com'),
-)
-
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/var/www/example.com/static/"
+STATIC_ROOT = choose_settings.STATIC_ROOT
+# URL prefix for static files.
+# Example: "http://example.com/static/", "http://static.example.com/"
+STATIC_URL = choose_settings.STATIC_URL
 
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # managesys
 ##使用Django搭建的数据管理系统
 ###提供数据整合，分析，以及分析结果展示
+
+###Setting up Django and your web server with uWSGI and nginx
+###https://uwsgi-docs.readthedocs.io/en/latest/tutorials/Django_and_nginx.html
