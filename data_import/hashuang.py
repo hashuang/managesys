afb29d1706cbd94e0577a175079d69f38299fa24
unknown <674062635@qq.com>
2016-12-05 18:16:32 +0800

    'hashuang'

--- data_import/hashuang.py
+++ data_import/hashuang.py
@@ -3,12 +3,35 @@ from pandas import DataFrame
 import pandas as pd
 import numpy as np
 import math
+from . import views
+from django.shortcuts import render
+from django.http import HttpResponse,HttpResponseRedirect,StreamingHttpResponse
+import os
 
 def Wushu(x):
     L=np.percentile(x,25)-1.5*(np.percentile(x,75)-np.percentile(x,25))
     U=np.percentile(x,75)+1.5*(np.percentile(x,75)-np.percentile(x,25))
     return x[(x<U)&(x>L)]	
-def num(request):
+def num_describe(scrapy_records,bookno):
+	print("hello");
+	if bookno=='"AS"':
+		bookno=bookno.split('"')[1]
+	#value=bookno.split('"')
+	#for i in range(len(value)):
+		#value=value[i]
+		#if value!=None:
+			#print(value)
+			#bookno=value
+	#print(bookno)
+	for i in range(len(scrapy_records)):
+		value = scrapy_records[i].get(bookno,None)
+		if value != None :
+			scrapy_records[i][bookno] = float(value)
+	frame=DataFrame(scrapy_records)
+	print(frame)	
+	df=frame.sort_values(by=bookno)
+	dfr=df[df>0].dropna(how='any')
+	#print(dfr[bookno].dtype)
 	clean=Wushu(dfr[bookno])
 	if clean is not None:
 		bc=(clean.max()-clean.min())/10
@@ -24,16 +47,16 @@ def num(request):
 	desx=[ele for ele in describe.index]
 	desy=[ele for ele in describe]
 	numy1=["%.2f%%"%(n*100) for n in numy]
-	contentVO={
-		'title':'测试',
-		'state':'success'
-	}
+	#contentVO={
+		#'title':'测试',
+		#'state':'success'
+	#}
 	ana_result={}
 	ana_result['scope']=numx
 	ana_result['num']=numy
-	contentVO['result']=ana_result
+	#contentVO['result']=ana_result
 	ana_describe={}
 	ana_describe['scopeb']=desx
 	ana_describe['numb']=desy
-	contentVO['describe']=ana_describe
+	#contentVO['describe']=ana_describe
 	return ana_result,ana_describe
--- data_import/views.py
+++ data_import/views.py
@@ -507,51 +507,25 @@ def echarts(request):
 def Wushu(x):
     L=np.percentile(x,25)-1.5*(np.percentile(x,75)-np.percentile(x,25))
     U=np.percentile(x,75)+1.5*(np.percentile(x,75)-np.percentile(x,25))
-    return x[(x<U)&(x>L)]	
+    return x[(x<U)&(x>L)]
+from . import hashuang    	
 def num(request):
 	print("success")
 	bookno=request.POST.get("bookno").upper();
-	#print(bookno)
 	sqlVO={}
 	sqlVO["db_name"]="l2own"
 	sqlVO["sql"]="SELECT HEAT_NO,"+bookno+" FROM qg_user.PRO_BOF_HIS_ALLFIELDS"
 	scrapy_records=models.BaseManage().direct_select_query_sqlVO(sqlVO)
-	#print(scrapy_records[:5])
-	for i in range(len(scrapy_records)):
-		value = scrapy_records[i].get(bookno,None)
-		if value != None :
-			scrapy_records[i][bookno] = float(value)
-	frame=DataFrame(scrapy_records)
-	df=frame.sort_values(by=bookno)
-	dfr=df[df>0].dropna(how='any')
-	#print(dfr[bookno].dtype)
-	clean=Wushu(dfr[bookno])
-	if clean is not None:
-		bc=(clean.max()-clean.min())/10
-		bcq=math.ceil(bc*1000)/1000
-		try:
-			section=pd.cut(clean,math.ceil((clean.max()-clean.min())/bcq+1))
-			end=pd.value_counts(section,sort=False)/clean.count()
-			describe=clean.describe()
-		except ValueError as e:
-		 	print(e)
-	numx=[ele for ele in end.index]
-	numy=[ele for ele in end]
-	desx=[ele for ele in describe.index]
-	desy=[ele for ele in describe]
-	numy1=["%.2f%%"%(n*100) for n in numy]
+	print(bookno)
+	print(scrapy_records[:5])
 	contentVO={
 		'title':'测试',
-		'state':'success'
+		'state':'success',
 	}
-	ana_result={}
-	ana_result['scope']=numx
-	ana_result['num']=numy
+	ana_result,ana_describe=hashuang.num_describe(scrapy_records,bookno)
 	contentVO['result']=ana_result
-	ana_describe={}
-	ana_describe['scopeb']=desx
-	ana_describe['numb']=desy
 	contentVO['describe']=ana_describe
+	print(ana_describe)
 	return HttpResponse(json.dumps(contentVO),content_type='application/json')
 
 from . import zhuanlu	
