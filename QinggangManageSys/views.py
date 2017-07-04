331a4315c22827a382cb3334d069f83cac8d72ed
changxincheng <changxincheng1019@gmail.com>
2017-07-04 09:47:36 +0800

    添加定时任务模块django_crontab

--- QinggangManageSys/views.py
+++ QinggangManageSys/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render, render_to_response
 
 def handler404(request):
     return render(request, '404.html', status=404)
+
+def paralle_test1():
+    with open('/Users/changxin/mission/qinggang/QinggangManageSys/QinggangManageSys/test_crontab.txt','a') as f:
+        f.write("hello\n")
